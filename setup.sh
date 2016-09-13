fb3c49147df916284e119c53ad8ec3afa3574e55
Ben Sheffield <bshefb@gmail.com>
2016-09-13 18:05:46 +0100

    Fixed typo

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 
 # Call source setup.sh to setup the aliases in your terminal
 
-alist build="go build -o build/compiler"
+alias build="go build -o build/compiler"
 alias buildn="go build -o build/compiler -tags 'nollvm'"
 alias compile="build/compiler"
 alias run="lli build/ben.ll"
