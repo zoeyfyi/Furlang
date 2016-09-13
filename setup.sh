43bad4cae0390b21d991abeba640386c4ba04edb
Ben Sheffield <bshefb@gmail.com>
2016-09-13 18:06:43 +0100

    Fixed missing llvm tag

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 
 # Call source setup.sh to setup the aliases in your terminal
 
-alias build="go build -o build/compiler"
+alias build="go build -o build/compiler -tags 'llvm'"
 alias buildn="go build -o build/compiler -tags 'nollvm'"
 alias compile="build/compiler"
 alias run="lli build/ben.ll"
