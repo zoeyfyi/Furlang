7038ceb5244a4a27ee48783a976384c8e5f8a318
Ben Sheffield <bshefb@gmail.com>
2016-09-13 18:05:15 +0100

    Added shortcut to build with llvm

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 
 # Call source setup.sh to setup the aliases in your terminal
 
-alias build="go build -o build/compiler -tags 'nollvm'"
+alist build="go build -o build/compiler"
+alias buildn="go build -o build/compiler -tags 'nollvm'"
 alias compile="build/compiler"
 alias run="lli build/ben.ll"
 alias ret="echo \$?"
