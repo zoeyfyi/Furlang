e1c23a94fd6242c978100800da23be32b6571010
Ben Sheffield <bshefb@gmail.com>
2016-09-18 18:28:56 +0100

    Dockerised building and testing

--- setup.sh
+++ setup.sh
@@ -3,8 +3,10 @@
 
 # Call source setup.sh to setup the aliases in your terminal
 
-alias build="go build -o build/compiler -tags 'llvm'"
-alias buildn="go build -o build/compiler -tags 'nollvm'"
-alias compile="build/compiler"
+alias build="go build -o furlang -tags 'llvm'"
+alias buildn="go build -o furlang -tags 'nollvm'"
+alias compile="sudo ./furlang"
 alias run="lli build/ben.ll"
 alias ret="echo \$?"
+alias dockerbuild="docker build -t furlang ."
+alias test="docker run -v $GOPATH/src/bitbucket.com/bongo227/furlang:/go/src/bitbucket.com/bongo227/furlang furlang"
