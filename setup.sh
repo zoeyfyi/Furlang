20434d80591dbb5aadc5a18ca18d353d9528c203
Ben Sheffield <bshefb@gmail.com>
2016-10-24 21:13:45 +0100

    Cleaned up old docker files and removed old aliases from setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,6 @@
 
 # Call source setup.sh to setup the aliases in your terminal
 
-alias build="go build -o furlang"
 alias clean="rm build/* && rm furlang"
-alias compile="./furlang"
 alias run="lli build/ben.ll"
 alias ret="echo \$?"
-alias dockerbuild="docker build -t furlang ."
-alias testg="go test -tags 'nollvm' ./..."
-alias test="docker run -v $GOPATH/src/bitbucket.com/bongo227/furlang:/go/src/bitbucket.com/bongo227/furlang furlang"
