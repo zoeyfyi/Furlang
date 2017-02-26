e156c2fa98a7405107e3a6cf4a8d1dae91ea0f9c
Ben Sheffield <bshefb@gmail.com>
2017-02-26 00:34:47 +0000

    Added other array tests.

--- irgen/irgen_test.go
+++ irgen/irgen_test.go
@@ -62,8 +62,8 @@ func TestIrgen(t *testing.T) {
 
 	files := []string{
 		"array_i32.fur",
-		// "array_i64.fur",
-		// "array_int.fur",
+		"array_i64.fur",
+		"array_int.fur",
 		"blocks.fur",
 		"cast_from_f32_type.fur",
 		"cast_from_f64_type.fur",
